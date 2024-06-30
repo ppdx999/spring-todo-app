b54e9da9020c217407c5476f95a9818b3b0feddd
fujis <ppdx999@gmail.com>
2024-06-30 17:20:27 +0900

    redirect / page to /todos

---
+++ src/main/java/com/example/demo/controller/web/RootWebController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootWebController {
+
+    @GetMapping("/")
+    public String redirectToTodos() {
+        return "redirect:/todos";
+    }
+}
