09fa7fedd22ac54e10163bdce5ea964104f06a22
fujis <ppdx999@gmail.com>
2024-07-07 13:10:21 +0900

    implement user create command

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,47 @@
 package com.example.demo;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.example.demo.command.UserCommands;
 
 @SpringBootApplication
 public class DemoApplication {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		if (args.length > 0 && "cmd".equals(args[0])) {
+			if (args.length < 1) {
+				System.err.println("Usage: cmd <command> [<args>]");
+				return;
+			}
+
+			String cmdName = args[1];
+
+			SpringApplication app = new SpringApplication(DemoApplication.class);
+			app.setWebApplicationType(WebApplicationType.NONE);
+			ConfigurableApplicationContext context = app.run(args);
+
+			/*
+			 * TODO: Create abstract Command class with run method and have all commands
+			 * Should Keep main method clean and simple
+			 */
+			switch (cmdName) {
+				case "create-user":
+					UserCommands cmd = context.getBean(UserCommands.class);
+					args = Arrays.copyOfRange(args, 2, args.length);
+					cmd.run(args);
+					break;
+				default:
+					System.err.println("Unknown command: " + cmdName);
+					break;
+			}
+
+			return;
+		}
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
---
+++ src/main/java/com/example/demo/command/UserCommands.java
@@ -0,0 +1,29 @@
+package com.example.demo.command;
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserCommands {
+
+    @Autowired
+    private UserService userService;
+
+    public void run(String... args) throws Exception {
+        if (args.length < 2) {
+            System.err.println("Usage: create-user <username> <password>");
+            return;
+        }
+        createUser(args[0], args[1]);
+    }
+
+    private void createUser(String username, String password) {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        userService.save(user);
+        System.out.println("User created successfully: " + username);
+    }
+}
