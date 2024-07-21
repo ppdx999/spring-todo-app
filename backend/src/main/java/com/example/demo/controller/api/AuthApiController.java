8744c740bbe23c915e8e1d501a1be7df0f606ab5
fujis <ppdx999@gmail.com>
2024-07-21 18:23:33 +0900

    response valid json on /api/auth/token

--- backend/src/main/java/com/example/demo/controller/api/AuthApiController.java
+++ backend/src/main/java/com/example/demo/controller/api/AuthApiController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,10 +17,31 @@ public class AuthApiController {
     private TokenService tokenService;
 
     @PostMapping("/token")
-    public String generateToken(Authentication authentication) {
-        return tokenService.generateToken(authentication);
+    public ResponseEntity<TokenResponse> generateToken(Authentication authentication) {
+        var token = tokenService.generateToken(authentication);
+        return ResponseEntity.ok(new TokenResponse(token));
     }
 
     @GetMapping("/check")
     public void checkToken() {}
+
+
+    private static class TokenResponse {
+        private String token;
+
+        public TokenResponse(String token) {
+            this.token = token;
+        }
+
+        
+        @SuppressWarnings("unused")
+        public String getToken() {
+            return token;
+        }
+
+        @SuppressWarnings("unused")
+        public void setToken(String token) {
+            this.token = token;
+        }
+    }
 }
