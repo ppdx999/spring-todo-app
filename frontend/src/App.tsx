a2cf3e8b4c4dce8a04909f9d89176b720f4ed9e7
fujis <ppdx999@gmail.com>
2024-07-21 18:25:12 +0900

    frontend: minimal implementation of data fetching powerd by openapi-fetch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,21 @@
-import { useState } from 'react'
+import { api } from "./api"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1 className='text-4xl font-bold text-center mb-2'>Welcome to Vite</h1>
       <div className='text-center'>
         <button
           className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-          onClick={() => setCount((count) => count + 1)}
+          onClick={() =>
+            api.POST('/api/auth/token', {
+              headers: {
+                Authorization: 'Basic ' + btoa('fujis:password'),
+              }
+            }).then((res) => console.log(res))
+          }
         >
-          count is {count}
+          login
         </button>
       </div>
     </>
